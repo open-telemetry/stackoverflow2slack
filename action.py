a6a13e36b520aa66e2dd086faea8970652c18e1c
Jacob Aronoff <jacobaronoff45@gmail.com>
2023-12-06 17:44:28 -0500

    state

--- action.py
+++ action.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 STACK_OVERFLOW_API = "https://api.stackexchange.com/2.3/search"
 SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
-STATE_FILE = "state.txt"
+STATE_FILE = "last.txt"
 
 def get_latest_question_timestamp():
     try:
